d251511ddac318514a8c715bb0098624123c3711
Shams Kasozi <schem208@gmail.com>
2022-06-11 18:20:37 +0300

    Adding view and pure function

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,3 +1,28 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.14; // this is the solidity version to be used
+contract SimpleStorage{
+    // Premitive types in solidity include used to define variables
+    // boolean, uint, int, address, bytes
+    //unassigned variable gets initialised to zero
+    //for example
+    // bool hasFavouriteNumber = true;
+    // uint256 FavouriteNumber = 5;
+    // string favouriteNumberInText = "Five";
+    // int256 negativeFavouriteInt = -5; 
+    // address myAddress = 0xF53C57B1012A4d6b6Eb555641aB77109F7370F20;
+    // bytes32 favouriteBytes = "cat";
+
+    uint256 favouriteNumber;
+
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    //view and pure functions do not spend any gas
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+
+}
+//0xd9145CCE52D386f254917e481eB44e9943F39138
