baf95dbcea99ecc2cc94139a8129917317425283
Shams Kasozi <schem208@gmail.com>
2022-06-12 17:43:05 +0300

    Learning about getting address and ABI to interact with contract.

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -13,4 +13,18 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function StorageFactoryStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //in order to interact with any contract, you need the 
+        //'address' and the 'ABI - Application Binary Interface' of that contract
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    //function that reads from the simpleStorage contract to the storageFactory
+    function storageFactoryGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
+
 }
