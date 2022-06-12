de760fe0a562f552d352c8a3fda0f5f6eeb0e86c
Shams Kasozi <schem208@gmail.com>
2022-06-12 07:44:39 +0300

    Adding comments after the second chapter

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,11 +2,17 @@
 
 pragma solidity ^0.8.14; // this is the solidity version to be used
 
+//EVM, Etherium Virtual Machine
+//Avalanche, Fantom, Polygon are some other networks that we can use
+
 contract SimpleStorage{
+    //A contract is similar to a class in other languages
     // Premitive types in solidity include used to define variables
     // boolean, uint, int, address, bytes
     //unassigned variable gets initialised to zero
     //for example
+
+    //some of the types in solidity.....
     // bool hasFavouriteNumber = true;
     // uint256 FavouriteNumber = 5;
     // string favouriteNumberInText = "Five";
@@ -16,14 +22,17 @@ contract SimpleStorage{
 
     uint256 favouriteNumber;
 
-    //mapping variable, used to get one's favorite number given the name
+    //you can create dictionaries like in NoSql/relations like in SQL/mappings
+    //or hash tables like this, using the 'mapping' function. which when you give
+    //it a key, it will spit out the value that the key represents
     mapping(string => uint256) public nameToFavouriteNumber;
 
+    //you can create a new type in solidity by using the 'struct' keyword
     struct People{
         uint256 favouriteNumber;
         string name;
     }
-
+    //you can create arrays or lists in solidity like this
     People[] public people;
 
     function store(uint256 _favouriteNumber) public {
@@ -31,10 +40,19 @@ contract SimpleStorage{
     }
 
     //view and pure functions do not spend any gas
+    //this is the function that does not modify the state of the contract
     function retrieve() public view returns(uint256){
         return favouriteNumber; 
     }
 
+    //you can create functions that modify the state of the blockchain like this
+    //we can also specify different data locations in our functions using keywords
+    //like calldata, 'memory' and 'storage'
+
+    //'calldata' and 'memory', mean that the data is only temporary and only exists for 
+    //the duration of the function
+
+    //'storage' variables are permanent and stay there for ever
     function addPerson(string memory _name, uint256 _favouriteNumber) public{
         people.push(People(_favouriteNumber, _name));
         nameToFavouriteNumber[_name] = _favouriteNumber;
