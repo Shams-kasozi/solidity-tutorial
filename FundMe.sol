677a74429f74975e5ccbc13cf9f63f810963ec67
Shams Kasozi <schem208@gmail.com>
2022-06-14 02:56:01 +0300

    The FundMe function now works properly, with limits and a record of those who have donated

--- FundMe.sol
+++ FundMe.sol
@@ -2,20 +2,30 @@
 
 pragma solidity ^0.8.8;
 
-import "./AggregatorV3Interface.sol";//this is an interface gotten from https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
+import "./AggregatorV3Interface.sol";
 //we need to get funds from users
 //withdraw finds
 //set a minimum funding value in USD
 
 contract FundMe {
 
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 50 * 1e18; //1 * 10 ** 18
+
+    //this array would keep the funders to have donated
+    address[] public funders;
+
+    //you could also keep a list of those who donated in this mapping
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable{
         //1. How do we send ETH to this contract?
         //we use 'msg.value' to set the value of how much someone has to send
-        require(msg.value >= minimumUsd, "Didn't send enough ETH"); 
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH"); 
         //1e18 == 1 * 10^18 == 1000000000000000000, thats 1 with 18 zeros
+
+        //here we are adding those who have donated to the funders array
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
     function getPrice() public view returns(uint256){
