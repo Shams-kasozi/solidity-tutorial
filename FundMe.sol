f2f3455886ece67094a251651e43cdfe9f8d86c8
Shams Kasozi <schem208@gmail.com>
2022-06-16 08:59:12 +0300

    using the 'constant' and 'immutable' keywords

--- FundMe.sol
+++ FundMe.sol
@@ -4,28 +4,30 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+error notOwner();
+
 contract FundMe {
 
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18; //1 * 10 ** 18
+    //the constant keyword will make the MINIMUM_USD variable more gas efficient
+    uint256 public constant MINIMUM_USD = 50 * 1e18; //1 * 10 ** 18
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
 
-    //to avoid anyone withdrawing our funds, we r gonna a 'modifier' where only 
-    //the owner can withdraw the funds from our account using this global variable called 'owner'
-    address public owner;
+    //using the immutable and constant keywords to make our contract more gas efficient
+    address public immutable i_owner;
 
     //then we create a constructor where we assign the 'owner' variable to the sender
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     //our funding method is now complete
     function fund() public payable{
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough ETH"); 
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH"); 
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -46,7 +48,11 @@ contract FundMe {
 
     //this 'modifier' will tell the 'withdraw' function to run this code first
     modifier OnlyOwner {
-        require(msg.sender == owner, "Sender is not the owner!");
+        require(msg.sender == i_owner, "Sender is not the owner!");
+
+        //we can use an 'if' statement with a 'revert' keyword instead of the 'require' keyword 
+        //for more efficiency
+        if(msg.sender != i_owner){ revert notOwner();}
         _;
     }
 }
