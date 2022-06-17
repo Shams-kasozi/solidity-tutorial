927b7806d4af9346ca37d9eec06f0f1ec58f9e27
Shams Kasozi <schem208@gmail.com>
2022-06-17 23:39:35 +0300

    Interacting with contracts

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -7,20 +7,17 @@ async function main(){
     const wallet = new ethers.Wallet("5f742921a1c4fc56c30f48b5fe3611fa981906dba4ea6f2a2a7deaa26066b6a8", provider)
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8")
+
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
     console.log("Deploying, Please wait...")
     const contract = await contractFactory.deploy();//STOP and wait for the contract to deploy
+    await contract.deployTransaction.wait(1)
 
-    //we can wait on block to make sure it gets attached to the chain
-    //can be termed as a transaction receipt
-    const transactionReceipt = await contract.deployTransaction.wait(1)
-        console.log("Here is the deployment transaction:")
-        console.log(contract.deployTransaction)
-        console.log("Here is the transaction receipt:")
-    console.log(transactionReceipt)
-    console.log(contract)
+    //Interacting with contracts. i.e. calling the methods on our contract SimpleStorage
+    //getting the favourite number
+    const currentFavouriteNumber = await contract.retrieve()
+    console.log(`Current favourite Number is: ${currentFavouriteNumber.toString()}`)
 }
-
 main()
     .then(() => process.exit(0))
     .catch((error) => {
