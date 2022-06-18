c12c74420b61bcc0632b88ac6a6065b8c53fa98b
Shams Kasozi <schem208@gmail.com>
2022-06-18 07:37:41 +0300

    encrypting private keys, not to appear in our code

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -1,10 +1,19 @@
 const ethers = require("ethers");
 const fs = require("fs-extra")
+require('dotenv').config()
 
 async function main(){
     //http:0.0.0.0:8545
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545")
-    const wallet = new ethers.Wallet("5f742921a1c4fc56c30f48b5fe3611fa981906dba4ea6f2a2a7deaa26066b6a8", provider)
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+    //changing the way we access our private key
+    const encryptedJson = fs.readFileSync("./encryptedKey.json", "utf8")
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
+        )
+    wallet = await wallet.connect(provider)
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8")
 
@@ -17,6 +26,15 @@ async function main(){
     //getting the favourite number
     const currentFavouriteNumber = await contract.retrieve()
     console.log(`Current favourite Number is: ${currentFavouriteNumber.toString()}`)
+
+    //calling the store method on our contract to store a number
+    //when you call a function/method on a contract, you get a transaction response
+    const transactionResponse = await contract.store("7")
+    //when you wait for the transaction to finish, you get a transaction receipt
+    const transactionReceipt = await transactionResponse.wait(1)
+
+    const updatedFavoriteNumber = await contract.retrieve()
+    console.log(`Updated favorite number is: ${updatedFavoriteNumber}`)
 }
 main()
     .then(() => process.exit(0))
