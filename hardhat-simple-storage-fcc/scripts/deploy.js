0e0a900a4e740f3b960dff77b50ab3f4ec7bb21d
Shams Kasozi <schem208@gmail.com>
2022-06-22 22:32:51 +0300

    Deploying simpleStorage from hardhat.

---
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -0,0 +1,21 @@
+//imports
+const { ethers } = require("hardhat")
+
+//async main
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    "SimpleStorage"
+  )
+  console.log("Deploying contarct...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed()
+  console.log(`deployed contract to: ${simpleStorage.address}`)
+}
+
+//main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1 )
+  })
