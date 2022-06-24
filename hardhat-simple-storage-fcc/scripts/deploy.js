1e958e8eb28337ac0f40334dc88b7975318a4d6d
Shams Kasozi <schem208@gmail.com>
2022-06-24 22:12:04 +0300

    verifying our contracts programatically on etherscan

--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,5 +1,5 @@
 //imports
-const { ethers } = require("hardhat")
+const { ethers, run, network} = require("hardhat")
 
 //async main
 async function main() {
@@ -10,6 +10,38 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`deployed contract to: ${simpleStorage.address}`)
+
+  //if we are on a testnet, this verifies our contract
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+  
+  //checks for the innital value of our favourite number
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current value is: ${currentValue}`)
+
+  //update the current value of our favourite number
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  }catch(error){
+    if (error.message.toLowerCase().includes("already verified")){
+      console.log("Already verified!")
+    }else {
+      console.log(error)
+    }
+  } 
 }
 
 //main
